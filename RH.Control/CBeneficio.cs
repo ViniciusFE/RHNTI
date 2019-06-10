576a3b2f047711449be3a82467be0db86b6772ff
ViniciusFE <viniciuseugenio.contato@hotmail.com>
2019-06-10 17:39:04 -0300

    Cbeneficio

--- RH.Control/CBeneficio.cs
+++ RH.Control/CBeneficio.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,31 @@ namespace RH.Control
 {
     public class CBeneficio
     {
-       
+        private RepositorieBeneficio _RepositorieBeneficio;
+
+        public CBeneficio()
+        {
+            _RepositorieBeneficio = new RepositorieBeneficio();
+        }
+
+        public List<Beneficio> SelecionarTodosBeneficios()
+        {
+            return _RepositorieBeneficio.SelecionarTodosBeneficios();
+        }
+
+        public Beneficio SelecionarBeneficio(int id)
+        {
+            return _RepositorieBeneficio.SelecionarBeneficio(id);
+        }
+
+        public void CadastrarBeneficio(Beneficio oBeneficio)
+        {
+            _RepositorieBeneficio.CadastrarBeneficio(oBeneficio);
+        }
+
+        public void AlterarBeneficio(Beneficio oBeneficio)
+        {
+            _RepositorieBeneficio.AlterarBeneficio(oBeneficio);
+        }
     }
 }
