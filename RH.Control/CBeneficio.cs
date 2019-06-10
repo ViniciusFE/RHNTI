aa05b679896dc54fce3cf46505c3fee912143c39
Vinicius Eugenio <vinicius_eugenio07@hotmail.com>
2019-06-10 17:13:39 -0300

    master

--- RH.Control/CBeneficio.cs
+++ RH.Control/CBeneficio.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RH.Model;
+using RH.Model.Repositories;
 
 namespace RH.Control
 {
-    class CBeneficio
+    public class CBeneficio
     {
+       
     }
 }
---
+++ RH.Model/Repositories/RepositorieDemissao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RH.Model.Repositories
+{
+    public class RepositorieDemissao
+    {
+        private RHEntities odb;
+
+        public RepositorieDemissao()
+        {
+            odb = Helper.Connection.GetConnection();
+        }
+
+        public RepositorieDemissao(RHEntities _odb)
+        {
+            odb = _odb;
+        }
+
+        public void CadastrarDemissao(Demissao aDemissao)
+        {
+            odb.Entry(aDemissao).State = System.Data.Entity.EntityState.Added;
+            odb.SaveChanges();
+        }
+    }
+}
