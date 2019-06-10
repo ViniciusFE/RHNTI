d4447a94e7e28aaa562996b7d1fa378561ad74c3
ViniciusFE <viniciuseugenio.contato@hotmail.com>
2019-06-10 17:03:31 -0300

    master

--- RH.Model/Repositories/RepositorieBeneficio.cs
+++ RH.Model/Repositories/RepositorieBeneficio.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace RH.Model.Repositories
 {
-    class RepositorieBeneficio
+    public class RepositorieBeneficio
     {
+        private RHEntities odb;
+
+        public RepositorieBeneficio()
+        {
+            odb = Helper.Connection.GetConnection();
+        }
+
+        public RepositorieBeneficio(RHEntities _odb)
+        {
+            odb = _odb;
+        }
+
+        public List<Beneficio> SelecionarTodosBeneficios()
+        {
+            return odb.Beneficio.Where(p => p.Ben_Situation == true).OrderBy(p => p.Ben_ID).ToList();
+        }
+
+        public Beneficio SelecionarBeneficio(int id)
+        {
+            return odb.Beneficio.Where(p => p.Ben_ID.Equals(id) && p.Ben_Situation == true).FirstOrDefault();
+        }
+
+        public void CadastrarBeneficio(Beneficio oBeneficio)
+        {
+            odb.Entry(oBeneficio).State = System.Data.Entity.EntityState.Added;
+            odb.SaveChanges();
+        }
+
+        public void AlterarBeneficio(Beneficio oBeneficio)
+        {
+            odb.Entry(oBeneficio).State = System.Data.Entity.EntityState.Modified;
+            odb.SaveChanges();
+        }
     }
 }
