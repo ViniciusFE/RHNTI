73f0289cb0534e1556ccd4686ba29fcdec7d2ae3
pedro lucas <pedro_leoti@hotmail.com>
2019-06-12 15:38:18 -0300

    Cadastrar Aluno

---
+++ RH.Model/Annotations/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace RH.Model
+{
+    [MetadataType(typeof(MD_Aluno))]
+    public partial class Aluno
+    {
+        internal class MD_Aluno
+        {
+
+            [DisplayName("Nome")]
+            [Required(ErrorMessage = "Por favor informe seu nome")]
+            public string Alu_Nome { get; set; }
+
+
+            [DisplayName("Matrícula")]
+            [Required(ErrorMessage = "Por favor informe sua matrícula")]
+            public int Alu_Matricula { get; set; }
+
+            [DisplayName("Série")]
+            [Required(ErrorMessage = "Por favor informe sua série")]
+            public int Alu_Serie { get; set; }
+
+            [DisplayName("Email")]
+            [Required(ErrorMessage = "Por favor informe seu email")]
+            [DataType(DataType.EmailAddress, ErrorMessage = "Errou", ErrorMessageResourceName = "Errou")]
+            public string Alu_Email { get; set; }
+
+            [DisplayName("Senha")]
+            [Required(ErrorMessage = "Por favor informe uma senha")]
+            [DataType(DataType.Password)]
+            public string Alu_Senha { get; set; }
+
+
+
+        }
+    }
+}
---
+++ RH.View/Controllers/AlunoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RH.Model;
+using RH.Control;
+
+namespace RH.View.Controllers
+{
+    public class AlunoController : Controller
+    {
+        CAluno _Control;
+        private RHEntities x = Model.Helper.Connection.GetConnection();
+
+        public AlunoController()
+        {
+            _Control = new CAluno();
+        }
+
+        // GET: Aluno
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CadasrarAluno()
+        {
+
+            List<Curso> cursos = (from p in x.Curso where p.Cur_Situation == true select p).ToList();
+            ViewBag.Alu_Curso_Cur_ID = new SelectList(cursos, "Cur_ID", "Cur_Nome");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CadasrarAluno(Aluno oAluno)
+        {
+            if (ModelState.IsValid)
+            {
+                oAluno.Alu_Situation = true;
+                oAluno.Alu_DataCadastro = DateTime.Now;
+                _Control.CadastrarAluno(oAluno);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
