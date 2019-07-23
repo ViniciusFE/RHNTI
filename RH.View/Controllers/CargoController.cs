6860668e9ab58f776e9bc773aeaa2b7237d41524
ViniciusFE <viniciuseugenio.contato@hotmail.com>
2019-07-23 17:16:42 -0300

    Cargo correção

--- RH.View/Controllers/CargoController.cs
+++ RH.View/Controllers/CargoController.cs
@@ -82,9 +82,11 @@ namespace RH.View.Controllers
         public ActionResult AlterarCargo(int id)
         {
             List<Setor> Setores = _Control.SelecionarSetoresEmpresa(Convert.ToInt32(Session["IDEmpresa"]));
-            ViewBag.Car_Setor_Set_ID = new SelectList(Setores, "Set_ID", "Set_Nome");
-
+            
             Cargo oCargo = _Control.SelecionarCargo(id);
+
+            ViewBag.Car_Setor_Set_ID = new SelectList(Setores, "Set_ID", "Set_Nome", oCargo.Car_Setor_Set_ID);
+
             return View(oCargo);
         }
 
@@ -93,16 +95,32 @@ namespace RH.View.Controllers
         public ActionResult AlterarCargo(Cargo oCargo,bool ChefeSetor)
         {
             List<Setor> Setores = _Control.SelecionarSetoresEmpresa(Convert.ToInt32(Session["IDEmpresa"]));
-            ViewBag.Car_Setor_Set_ID = new SelectList(Setores, "Set_ID", "Set_Nome");
+            ViewBag.Car_Setor_Set_ID = new SelectList(Setores, "Set_ID", "Set_Nome", oCargo.Car_Setor_Set_ID);
 
-            if(ModelState.IsValid)
+            if (ChefeSetor)
+            {
+                bool StatusChefe = _Control.SelecionarChefeSetor(oCargo.Car_Setor_Set_ID);
+                if (StatusChefe)
+                {
+                    ModelState.AddModelError("ChefeSetor", "Este setor já possui um chefe, troque o status de chefe do cargo ou selecione outro setor de atividade");
+                    return View();
+                }
+            }
+
+            bool CargosComMesmoNome = _Control.CargosComMesmoNome(oCargo.Car_Nome, oCargo.Car_Setor_Set_ID);
+            if (CargosComMesmoNome)
+            {
+                ModelState.AddModelError("Car_Nome", "Este setor já possui um cargo com este nome");
+                return View();
+            }
+
+
+            if (ModelState.IsValid)
             {
                 Cargo oCargo1 = _Control.SelecionarCargo(oCargo.Car_ID);
-
                 oCargo1.Car_Nome = oCargo.Car_Nome;
                 oCargo1.Car_Setor_Set_ID = oCargo.Car_Setor_Set_ID;
-                oCargo1.Car_Chefe = oCargo.Car_Chefe;
-
+                oCargo1.Car_Chefe = ChefeSetor;
                 _Control.AlterarCargo(oCargo1);
 
                 return RedirectToAction("Index");
