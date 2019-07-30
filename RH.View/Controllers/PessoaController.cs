4dd61cd2b1c41b735e32140ff3ac1364dcaa856e
thailajf <thailajf@hotmail.com>
2019-07-30 15:28:41 -0300

    -

--- RH.View/Controllers/PessoaController.cs
+++ RH.View/Controllers/PessoaController.cs
@@ -138,11 +138,11 @@ namespace RH.View.Controllers
             List<Cargo> Cargos = DbPessoa.SelecionarCargosEmpresa(IDEmpresa);
 
 
-            int qtd = 0;  //DbPessoa.BeneficiosEmpresa(IDEmpresa).Count();
-            //List<Beneficio> beneficios = DbPessoa.BeneficiosEmpresa(IDEmpresa);
-            //ViewBag.Beneficios = beneficios;
+            int qtd =DbPessoa.BeneficiosEmpresa(IDEmpresa).Count();
+            List<Beneficio> beneficios = DbPessoa.BeneficiosEmpresa(IDEmpresa);
+            ViewBag.Beneficios = beneficios;
             ViewBag.qtdB = qtd;
-            //ViewBag.IDE = IDEmpresa;
+            ViewBag.IDE = IDEmpresa;
 
             ViewBag.Setores = Setores;
             ViewBag.Cargos = Cargos;
